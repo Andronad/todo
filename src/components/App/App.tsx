b00a0a538b6a6300dd8c31491edebb9cdf2e43b5
Andronad <andronad.am@gmail.com>
2022-04-29 15:34:02 +0300

    Create SearchForm

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,11 +2,13 @@ import { Container } from "@mui/material";
 import Header from "../Header";
 import ToDoList from "./../ToDoList/ToDoList";
 import { list } from "./../../constants/index";
+import SearchForm from "../SearchForm";
 
 function App() {
     return (
         <Container maxWidth="xs">
             <Header />
+            <SearchForm />
             <ToDoList list={list} />
         </Container>
     );
