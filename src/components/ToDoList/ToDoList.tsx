39e8677f31149b7b242b239c1785415e0abab791
Andronad <andronad.am@gmail.com>
2022-05-05 15:04:28 +0300

    Show message if dont find anyting

--- src/components/ToDoList/ToDoList.tsx
+++ src/components/ToDoList/ToDoList.tsx
@@ -1,19 +1,27 @@
-import { Stack } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 import ToDoItem from "../ToDoItem/ToDoItem";
 import { useSelector } from "react-redux";
 
 const ToDoList = () => {
-    let list: IToDoItem[] = useSelector((state: ToDoState) => state.todos);
+    const list: IToDoItem[] = useSelector((state: ToDoState) => state.todos);
     const filter: string = useSelector((state: ToDoState) => state.filter);
 
-    list = list.filter((e) =>
+    const filteredList = list.filter((e) =>
         e.label.toLowerCase().includes(filter.toLowerCase())
     );
+
+    const showedList =
+        filteredList.length !== 0 ? (
+            filteredList.map((e) => {
+                return <ToDoItem item={e} key={e.id} />;
+            })
+        ) : (
+            <Typography variant="h4">Can't find anything :(</Typography>
+        );
+
     return (
         <Stack direction={"column"} textAlign="center" spacing={1}>
-            {list.map((e) => {
-                return <ToDoItem item={e} key={e.id} />;
-            })}
+            {showedList}
         </Stack>
     );
 };
